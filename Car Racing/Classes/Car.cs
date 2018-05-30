197b1c2c6867e91fc9f943a7745628c0dc52fbbc
ЖолдасхановУ <@223_14>
2018-05-30 21:01:19 +0600

    Add project files.

---
+++ Car Racing/Classes/Car.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Car_Racing.Classes
+{
+    /*●	Разработать абстрактный класс «автомобиль» (класс Car). 
+Собрать в нем все общие поля, свойства (например, скорость) методы (например, ехать). */
+    public delegate void Driving(Car car, float deistance);
+    public delegate void Finish(Car car);
+    public delegate void Ready(Car car);
+
+    public abstract class Car
+    {
+        protected Ready readyHandler;
+
+        public int Name { get; set; }
+        public float Speed { get; set; }
+        public float Distanse { get; set; }
+
+        public abstract void Drive(float time, float limit);
+
+        public abstract void GetReady();
+
+        public event Ready Ready // событие
+        {
+            add { readyHandler += value; }
+            remove { readyHandler -= value; }
+        }
+        public abstract event Finish Finish; // событие 
+        public abstract event Driving Driving; // событие
+
+    }
+}
---
+++ Car Racing/Classes/Game.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Car_Racing.Classes
+{
+    /*●	Класс игры должен производить запуск соревнований автомобилей, 
+выводить сообщения о текущем положении автомобилей, 
+выводить сообщение об автомобиле, победившем в гонках.*/
+
+    public class Game
+    {
+        int mile = 100;
+        private Car winner;
+
+        public void ReadyToStart(SportCar sp, SportCar sp2)
+        {
+            winner = (Car)null;
+            sp.Ready += CarReady;
+            sp2.Ready += CarReady;
+            sp.Driving += CarDriving;
+            sp2.Driving += CarDriving;
+            sp.Finish += CarFinish;
+            sp2.Finish += CarFinish;
+
+            Console.Clear();
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Get redy");
+
+            sp.GetReady();
+            sp2.GetReady();
+
+            Thread.Sleep(2000);
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Set");
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("GO!!!");
+            Thread.Sleep(800);
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.White;
+
+            for (float t = 0; ; t += 0.1F)
+            {
+                Console.Clear(); 
+
+                Console.WriteLine($"Time {TimeSpan.FromSeconds(t)}");
+                sp.Drive(t, 400);
+                sp2.Drive(t, 400);
+                Thread.Sleep(100);
+                if (winner != null)
+                    break;
+            }
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("The winner is " + winner.Name);
+        }
+
+        private void CarFinish(Car car)
+        {
+            winner = car;
+        }
+
+        private void CarDriving(Car car, float distance)
+        {
+            Console.WriteLine("Car " + car.Name + " distanse " + distance);
+        }
+
+        private void CarReady(Car car)
+        {
+            Console.WriteLine("The car " + car.Name + " Is ready");
+        }
+
+    }
+}
